2ba902b357bd1677702a6726ee81190d8758a48d
Alex <Alex@gmail.com>
2020-10-12 14:53:43 +0300

    first commit2

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
+// const express = require('express');
+// const app = express();
+// const bodyParser= require('body-parser');
+
+// const PORT = process.env.PORT || 3000;
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+// app.get('/', (req, res) => { 
+//   const str="I AM AN ENDPOINT FOR YOUR SALESFORCE APPLICATION";
+//   res.send(str);
+// });
+
+// app.listen(PORT, () => { 
+//   console.log(`Listening on ${ PORT }`);
+// });
+
+// app.post('/', (req, res) => { 
+//   // console.log(req.body);
+//   // const data=req.body;
+//   if(JSON.stringify(data) != '{}') {
+//     res.sendStatus(200);
+//     // res.send(data); //echoing the request data back as response 
+//   } else { 
+//     // res.send('No data Received');
+//     res.sendStatus(400);
+//   }
+// });
+
+// app.post("/test", (request, response) => {
+//   console.log(request.body);
+//   return response.sendStatus(200);
+// });
+
 const express = require('express');
 const app = express();
 const bodyParser= require('body-parser');
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000 ;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get('/', (req, res) => { 
@@ -11,22 +43,15 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => { 
-  console.log(`Listening on ${ PORT }`);
+  console.log(`Listening on ${ PORT }`); 
 });
 
 app.post('/', (req, res) => { 
-  // console.log(req.body);
-  // const data=req.body;
-  if(JSON.stringify(data) != '{}') {
-    res.sendStatus(200);
-    // res.send(data); //echoing the request data back as response 
-  } else { 
-    // res.send('No data Received');
-    res.sendStatus(400);
+  console.log(req.body);
+  const data=req.body;
+  if (JSON.stringify(data) != '{}') {
+    res.send(data); //echoing the request data back as response
+  } else {
+    res.send('No data Received');
   }
-});
-
-app.post("/test", (request, response) => {
-  console.log(request.body);
-  return response.sendStatus(200);
 });
