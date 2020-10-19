82134c67fa18598528bb990796075ed418223b57
Alex <Alex@gmail.com>
2020-10-19 11:00:27 +0300

    third commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mailer = require('./nodemailer');
 const { request, response } = require('express');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // for parsing application/json
 // app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
